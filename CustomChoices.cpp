018eea7d37ecc65bba2cd5f05e5e708f3abc34ff
Request_Full_Access <100245301+PA1515@users.noreply.github.com>
2023-02-07 07:47:38 -0800

    tired

--- CustomChoices.cpp
+++ CustomChoices.cpp
@@ -27,8 +27,14 @@ void gainAdminAccess()
 
 std::vector<CustomChoices> setupChoices()
 {
-	std::vector<CustomChoices> returnVec{};
-	CustomChoices admin("Gain administrator access", "Gains administrator access for the user (input username) if the program is in the [system32] directory. Only works at the sign-in screen after a restart, since Windows Defender accurately picks up that this program is privelege-escalation.", gainAdminAccess);
-	std::cout << '\n' << admin.getDescription() << '\n';
+	std::vector<CustomChoices> returnVec{}; //by the way, the [\] symbols are telling the compiler that everything here is one line, so we don't \
+	have to concatenate our strings!
+
+	CustomChoices admin("Gain administrator access", "Gains administrator access for the user (input username) if \
+the program is in the [system32] directory. Only works at the sign-in screen after a restart, since Windows \
+Defender accurately picks up that this program is privelege-escalation.", gainAdminAccess);
+	
+	CustomChoices 
+
 	return returnVec;
 }
